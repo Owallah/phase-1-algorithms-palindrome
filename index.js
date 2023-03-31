a899411445dd13d15fe20e9281aed0944b0f7e73
Johnson <mikesonowallah@gmail.com>
2023-03-31 12:16:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let replaceSpecialChars = /[\W_]/g;
+  let lowerCaseWord = word.toLowerCase()
+  //replace any special characters or space
+  lowerCaseWord.replace(replaceSpecialChars, '')
+
+  //split, reverse and join the word
+  let reversedWord = lowerCaseWord.split('').reverse().join('')
+
+  //check if the reversed word is equal to the original word...using strict equality operator
+  return reversedWord === lowerCaseWord;
 }
 
 /* 
   Add your pseudocode here
+  *convert word to lower case using .lowerCase()
+  *replace any special characters or space
+  *split the characters into an array
+  *reverse the split string
+  *join the reversed string
+  *return true if the joined string equals the lower cased string
 */
 
 /*
@@ -20,6 +36,22 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("neveroddoreven"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("MOm"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("nope"));
+
 }
 
 module.exports = isPalindrome;
